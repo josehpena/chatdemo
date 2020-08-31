32999c1f601fac1560eb89c335db15c41ed01c20
jose pena <josepena@planeta.inf.br>
2020-08-31 15:11:28 -0300

    Sistema de login pronto

--- app.js
+++ app.js
@@ -1,34 +1,52 @@
-var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
-var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+const passport = require('passport')
+const session = require('express-session')
+const MongoStore = require('connect-mongo')(session)
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-
+var index = require('./routes/index');
 var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use(express.static(path.join(__dirname, 'public')));
+require('./auth')(passport);
+app.use(session({
+  store: new MongoStore({
+    db: global.db,
+    ttl: 30 * 60 // = 30 minutos de sessão
+    , url: 'mongodb://localhost:27017/demo'
+  }),
+  secret: '123',//configure um segredo seu aqui
+  resave: false,
+  saveUninitialized: false
+}))
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.use('/', index);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
+app.use(function (req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,34 @@
 var express = require('express');
 var router = express.Router();
+const passport = require('passport');
+const { route } = require('../app');
 
+function authenticationMiddleware() {
+  return function (req, res, next) {
+    if (req.isAuthenticated()) {
+      return next()
+    }
+    res.redirect('/login?fail=true')
+  }
+}
 /* GET home page. */
 router.get('/', function (req, res, next) {
   res.render('login', { message: null });
 });
 
-router.get('/chat', function (req, res) {
-  res.render('chat', {});
+router.get('/chat', authenticationMiddleware(), function (req, res) {
+  res.render('chat', { username: req.user.username });
 });
+
+router.get('/login', () => {
+  if (req.query.fail)
+    res.render('login', { message: 'Usuário e/ou senha incorretos!' });
+  else
+    res.render('login', { message: null });
+});
+
+router.post('/login',
+  passport.authenticate('local', { successRedirect: '/chat', failureRedirect: '/login?fail=true' })
+);
 
 module.exports = router;
